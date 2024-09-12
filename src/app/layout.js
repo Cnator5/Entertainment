d0bc559f94799d2ea27e39631bf5ef2f61761565
Cnator <beyoh172@gmail.com>
2024-09-12 16:51:37 +0100

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/Components/Navbar/navBar";
 import Footer from "@/Components/Footer/footer";
 import 'tailwindcss/tailwind.css';
+import Navbar from './../Components/Navbar/NavBar';
 
 
 
